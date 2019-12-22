5beb734f2120888bc0aade81eb2ad0f9e6de929b
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2019-12-22 12:37:46 -0300

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <h1>This is a NuxtJS application that uses Typescript, Vuetify, Jest and SCSS.</h1>
+  </v-container>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+
+}
+</script>
